448dfbf4c44fed660ba5c4e26984c1deaf28b68c
Delson Lima <delsonlima.dev@gmail.com>
2022-07-04 18:00:39 -0300

    Adiciona Carrinho e Produto

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        testCarrinho();
+    }
+
+    public static void testCarrinho() {
+        Produto produto = new Produto();
+        produto.setCodigo(1389);
+        produto.setNome("Produto 1");
+        produto.setPreco(BigDecimal.valueOf(100));
+
+        Produto produto2 = new Produto();
+        produto2.setCodigo(238);
+        produto2.setNome("Produto 2");
+        produto2.setPreco(BigDecimal.valueOf(80));
+
+        Carrinho carrinho = new Carrinho(new ArrayList<>(), new BigDecimal(0), new Cliente());
+        carrinho.add(produto, 3);
+        carrinho.add(produto2, 1);
+
+        carrinho.listar();
+        System.out.println("Total: " + carrinho.getTotalCompra());
     }
 }
