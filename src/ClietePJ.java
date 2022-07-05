e44e1a00bd490905e44ec432167494e9ead41b9c
eric3119 <esc2@ic.ufal.br>
2022-07-05 18:11:19 -0300

    fix typo

--- src/ClietePJ.java
+++
@@ -1,9 +0,0 @@
-public class ClietePJ extends Pessoa{
-
-    private String cnpj;
-
-    public ClietePJ(String nome, int senha, String cnpj) {
-        super(nome, senha);
-        this.cnpj = cnpj;
-    }
-}
